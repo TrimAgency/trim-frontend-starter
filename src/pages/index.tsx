6fb0d1bd2c9f1384698bd50efe3de2db01fdc61e
fortdev210 <sharp.blader2050@gmail.com>
2022-12-28 17:48:03 +0800

    config prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
-import { Inter } from "@next/font/google";
-import Head from "next/head";
-import Image from "next/image";
-
-import styles from "../styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
     </>
-  );
+  )
 }
