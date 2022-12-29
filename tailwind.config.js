a6df450daeb4d61125e22a5bb32d0ba0dc867d89
fortdev210 <sharp.blader2050@gmail.com>
2022-12-29 18:04:30 +0800

    add tailwind screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xl': { min: '1536px' },
+        xl: { min: '1280px' },
+        lg: { min: '1024px' },
+        md: { min: '768px' },
+        sm: { min: '640px' },
+      },
+    },
   },
   plugins: [],
 }
